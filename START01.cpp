d55150b9e4851dea057ba96caa118f21378436c8
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2016-02-03 21:22:02 +0530

    Adding solution to START01

---
+++ START01.cpp
@@ -0,0 +1,16 @@
+/*
+ *  Author: Arpit Bhayani
+ *  http://arpitbbhayani.github.io
+ */
+#include <stdio.h>
+
+#define ll long long
+
+#define MIN(a, b) a < b ? a : b
+
+int main(int argc, char *argv[]) {
+    ll int n;
+    scanf("%lld", &n);
+    printf("%lld\n", n);
+    return 0;
+}
